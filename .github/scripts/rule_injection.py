624ba8f444d018a2224c786faa2a768884e3e091
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-11-06 18:34:24 +0000

    Add custom workflow for injecting CAPE's detection rules into the extractors

---
+++ .github/scripts/rule_injection.py
@@ -0,0 +1,12 @@
+import os
+
+import requests
+
+for root, dirs, files in os.walk('../../'):
+    for filename in files:
+        if filename.endswith('.py'):
+            extractor_name = filename[:-3]
+            resp = requests.get(f'https://raw.githubusercontent.com/kevoreilly/CAPEv2/refs/heads/master/data/yara/CAPE/{extractor_name}.yar')
+            if resp.ok:
+                with open(os.path.join(root, filename), 'a') as yar_file:
+                    yar_file.write(f'\ndetection_rule = """\n{resp.text}\n"""\n')
