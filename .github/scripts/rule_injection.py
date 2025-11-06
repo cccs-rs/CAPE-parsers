8c35d90729d125814bb422cc219c1671042e7751
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2025-11-06 19:39:09 +0000

    Escape slashes in rules when embedded in the extractor

--- .github/scripts/rule_injection.py
+++ .github/scripts/rule_injection.py
@@ -8,5 +8,6 @@ for root, dirs, files in os.walk('../../'):
             extractor_name = filename[:-3]
             resp = requests.get(f'https://raw.githubusercontent.com/kevoreilly/CAPEv2/refs/heads/master/data/yara/CAPE/{extractor_name}.yar')
             if resp.ok:
+                rule_content = resp.text.replace("\\", '\\\\')
                 with open(os.path.join(root, filename), 'a') as yar_file:
-                    yar_file.write(f'\ndetection_rule = """\n{resp.text}\n"""\n')
+                    yar_file.write(f'\ndetection_rule = """\n{rule_content}\n"""\n')
